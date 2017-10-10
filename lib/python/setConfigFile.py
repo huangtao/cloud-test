3f37fac51e846f48d0024f938cc4171c75c76661
guixianggui <GuixiangGui@boyaa.com>
2017-10-10 14:07:28 +0800

    commit modify

--- lib/python/setConfigFile.py
+++ lib/python/setConfigFile.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import configparser
-#import ConfigParser as configparser
 import sys
-#reload(sys)
-#sys.setdefaultencoding( "utf-8" )
+
 global args
 secs = sys.argv[3]
 if secs=='runtest':
@@ -17,11 +15,10 @@ params = {}
 for arg in args:
     strs = arg.split(':')
     params[strs[0]]=strs[1]
-print params
-print cfgFilePath
+
 cf = configparser.ConfigParser()
 cf.read(cfgFilePath, 'utf-8')
-#cf.read(cfgFilePath)
+
 for (key, value) in params.items():
   try:
     cf.set(secs, key, value)
